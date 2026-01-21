0ac38783b2e125ad3eeb5e345e17d29ff8fb1a83
Alexander Menzel <menzel.alexander@proton.me>
2026-01-21 14:55:06 +0100

    :technologist: Remove debug flags

--- Core/Inc/task_debug.h
+++ Core/Inc/task_debug.h
@@ -3,7 +3,7 @@
 
 /* Enable this to log thread creation/start details. Set to 0 to disable. */
 #ifndef OS_TASKS_DEBUG
-#define OS_TASKS_DEBUG 1
+#define OS_TASKS_DEBUG 0
 #endif
 
 #ifndef ERROR_HANDLER_ON_FAILURE
@@ -11,15 +11,15 @@
 #endif
 
 #ifndef INPUT_TASK_DEBUG_PRINTING
-#define INPUT_TASK_DEBUG_PRINTING 1
+#define INPUT_TASK_DEBUG_PRINTING 0
 #endif
 
 #ifndef SENSOR_TASK_DEBUG_PRINTING
-#define SENSOR_TASK_DEBUG_PRINTING 1
+#define SENSOR_TASK_DEBUG_PRINTING 0
 #endif
 
 #ifndef VIEW_PRESENTER_TASK_DEBUG_LEDS
-#define VIEW_PRESENTER_TASK_DEBUG_LEDS 1
+#define VIEW_PRESENTER_TASK_DEBUG_LEDS 0
 #endif
 
 #endif /* CORE_INC_TASK_DEBUG_H */
